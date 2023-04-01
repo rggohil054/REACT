790c0e016c574d6a176195ca43bb5b1c9bd6d719
rggohil054 <113237061+rggohil054@users.noreply.github.com>
2023-04-01 18:05:53 +0530

    Add files via upload

--- app/my-app/src/index.js
+++ app/my-app/src/index.js
@@ -1,33 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import Layout from './Layout';
+import Home from './Home';
+import AboutUs from './AboutUs';
+import ContactUs from './ContactUs';
+import Product from './Product'
+import Course from './Course'
+import { BrowserRouter,Routes, Route } from 'react-router-dom';
 const root = ReactDOM.createRoot(document.getElementById('root'));
-var num1 = 10
-var num2 = 3
-// we can use any mathematical expression in JSX
-var output = (<div className='container'>
-    <div className='row'>
-        <div className='col-6 offset-3'>
-            <div className='card shadow'>
-                <div className='card-header text-bg-primary'>
-                    <h3>Login</h3>
-                </div>
-                <div className='card-body'>
-                    <form>
-                        <div className='mb-3'>
-                            <input type='email' className='form-control' placeholder='Email' />
-                        </div>
-                        <div className='mb-3'>
-                            <input type='password' className='form-control'
-                            placeholder='Password' />
-                        </div>
-                        <div className='mb-3 text-end'>
-                            <input type='button' className='btn btn-primary' value='Login' />&nbsp;<input type='reset' className='btn btn-secondary' value='clear all' />
-                        </div>
-                    </form>
-                </div>
-            </div>
-        </div>
-    </div>
-</div>)
-root.render(output);
+function MyRouter()
+{
+    return(
+        <BrowserRouter>
+            <Routes>
+                <Route path='/' element={<Layout />}>
+                    <Route path='/home' element={<Home />} />
+                    <Route path='/aboutus' element={<AboutUs />} />
+                    <Route path='/product' element={<Product />} />
+                    <Route path='/courses' element={<Course />} />
+                    <Route path='/contact' element={<ContactUs />} />
+                </Route>
+            </Routes>
+        </BrowserRouter>
+    )
+}
+root.render(<MyRouter />);
